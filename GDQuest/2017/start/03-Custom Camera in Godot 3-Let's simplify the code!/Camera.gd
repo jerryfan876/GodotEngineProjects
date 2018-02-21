5dd507d7fe04daad6046330dfc1718dc2a1a946b
jerryfan876 <jerryfan876@gmail.com>
2018-02-20 21:13:07 -0800

    update day3

--- GDQuest/2017/start/03-Custom Camera in Godot 3-Let's simplify the code!/Camera.gd
+++ GDQuest/2017/start/03-Custom Camera in Godot 3-Let's simplify the code!/Camera.gd
@@ -1,21 +1,13 @@
 # Custom camera
 extends Node
 
-onready var screen_size = Vector2(Globals.get("display/width"), Globals.get("display/height"))
+onready var screen_size = OS.get_window_size()
 onready var player = get_node("Player")
-onready var last_player_pos = player.get_pos()
 
 func _ready():
-	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] = last_player_pos - screen_size / 2
-	get_viewport().set_canvas_transform(canvas_transform)
-
+	update_camera()
 
 func update_camera():
-	# This difference will cause rounding errors that build up over time if you don't force pixel snapping or round out the values. See the 3rd tutorial for a cleaner implementation.
-	var player_offset = last_player_pos - player.get_pos()
-	last_player_pos = player.get_pos()
-
 	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] += player_offset
+	canvas_transform[2] = -player.get_pos() + screen_size / 2
 	get_viewport().set_canvas_transform(canvas_transform)
