938ddb0a96fe810f601158c5ee5db85811ac1f5e
jerryfan876 <jerryfan876@gmail.com>
2018-02-21 01:51:04 -0800

    day 4 and 5

--- GDQuest/2017/start/04-Custom camera with instant room transitions/Camera.gd
+++ GDQuest/2017/start/04-Custom camera with instant room transitions/Camera.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 onready var window_size = OS.get_window_size()
 onready var player = get_node("Player")
-onready var player_world_pos = get_player_world_pos()
+onready var player_world_pos = get_player_grid_pos()
 
 
 func _ready():
@@ -11,10 +11,22 @@ func _ready():
 	get_viewport().set_canvas_transform(canvas_transform)
 
 
-func get_player_world_pos():
-	# Converts the player position to the map he's on. Returns the map id/coordinates as Vector2
-	return Vector2()
+func get_player_grid_pos():
+	var pos = player.get_pos()
+	var x = floor(pos.x / window_size.x)
+	var y = floor(pos.y / window_size.y)
+	return Vector2(x, y) 
 
 
 func update_camera():
-	pass
+	var new_player_grid_pos = get_player_grid_pos()
+	var transform = Matrix32()
+	
+	if new_player_grid_pos != player_world_pos:
+		player_world_pos = new_player_grid_pos - player.size / 2
+		transform = get_viewport().get_canvas_transform()
+		transform[2] = - player_world_pos * window_size
+		get_viewport().set_canvas_transform(transform)
+		
+	
+	return transform
