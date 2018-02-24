20378d86b2956f8c9d58d8b53ab689e0c8fb8197
jerryfan876 <jerryfan876@gmail.com>
2018-02-24 04:51:48 -0800

    30 day challenge, day 7, 8

--- GDQuest/2017/start/08-Basic grid system/Grid.gd
+++ GDQuest/2017/start/08-Basic grid system/Grid.gd
@@ -1,18 +1,35 @@
 extends TileMap
 
-var tile_size
-var half_tile_size
+enum ENTITY_TYPES {PLAYER, OBSTACLE, COLLECTIBLE}
 
-var grid_size
+var tile_size = get_cell_size()
+var half_tile_size = tile_size / 2
+
+var grid_size = Vector2(16,16)
 var grid = []
 
-# onready var Obstacle = preload("res://Obstacle.tscn")
+onready var Obstacle = preload("res://Obstacle.tscn")
 
 
 func _ready():
-	# 1. Create the grid Array
-	# 2. Create obstacles
-	pass
+	for x in range(grid_size.x):
+		grid.append([])
+		for y in range(grid_size.y):
+			grid[x].append(null)
+	
+	var positions = []
+	for n in range(5):
+		var grid_pos = Vector2(randi() % int(grid_size.x) , randi() % int(grid_size.y))
+		
+		if not grid_pos in positions:
+			positions.append(grid_pos)
+			
+	for pos in positions: 
+		var new_obstacle = Obstacle.instance()
+		new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
+		grid[pos.x][pos.y] = OBSTACLE
+		add_child(new_obstacle)
+	
 
 
 func is_cell_vacant():
