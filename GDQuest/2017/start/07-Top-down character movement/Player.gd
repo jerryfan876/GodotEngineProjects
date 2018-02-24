437c5e3ff0858f0f1f828acc48335e9c5bd97348
jerryfan876 <jerryfan876@gmail.com>
2018-02-24 04:23:42 -0800

    30day challenge

--- GDQuest/2017/start/07-Top-down character movement/Player.gd
+++ GDQuest/2017/start/07-Top-down character movement/Player.gd
@@ -1,9 +1,50 @@
 extends KinematicBody2D
 
+var direction  = Vector2()
+
+const UP = Vector2(0, -1)
+const RIGHT = Vector2(1, 0)
+const DOWN = Vector2(0, 1)
+const LEFT = Vector2(-1, 0)
+
+var speed = 0
+const MAX_SPEED = 400
+
 
 func _ready():
 	set_fixed_process(true)
 
 
 func _fixed_process(delta):
-	pass
+	var is_moving = Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")
+	direction = Vector2()
+	
+	if is_moving:
+		speed = MAX_SPEED
+		if Input.is_action_pressed("move_up"):
+			direction += UP
+		elif Input.is_action_pressed("move_down"):
+			direction += DOWN
+		if Input.is_action_pressed("move_left"):
+			direction += LEFT
+		elif Input.is_action_pressed("move_right"):
+			direction += RIGHT
+	else:
+		speed = 0
+	var velocity = speed * direction.normalized() * delta
+	
+	move(velocity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
